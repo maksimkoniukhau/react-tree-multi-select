b3d403cb9a2ce4d7f4eb10c3ce1662877a4a88e3
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2025-01-13 22:59:53 +0100

    Update icons, add FontAwesome license comments

--- src/FieldToggle.tsx
+++ src/FieldToggle.tsx
@@ -9,9 +9,16 @@ export const FieldToggle: FC<FieldToggleProps> = memo((props) => {
 
   return (
     <div {...props.componentAttributes}>
+      {/*Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
       <svg className="rts-field-toggle-icon" viewBox="0 0 448 512">
-        <path
-          d="M224 397.3l22.6-22.6 160-160L429.3 192 384 146.7l-22.6 22.6L224 306.7 86.6 169.4 64 146.7 18.7 192l22.6 22.6 160 160L224 397.3z"/>
+        {props.componentProps.expanded ? (
+          <path
+            d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/>
+
+        ) : (
+          <path
+            d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
+        )}
       </svg>
     </div>
   );
--- src/NodeToggle.tsx
+++ src/NodeToggle.tsx
@@ -13,10 +13,11 @@ export const NodeToggle: FC<NodeToggleProps> = memo((props) => {
 
   return (
     <div className={nodeToggleClasses} onClick={onClick}>
+      {/*Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.*/}
       <svg className="rts-node-toggle-icon" viewBox={expanded ? '0 0 448 512' : '0 0 320 512'}>
         {expanded ? (
           <path
-            d="M224 397.3l22.6-22.6 160-160L429.3 192 384 146.7l-22.6 22.6L224 306.7 86.6 169.4 64 146.7 18.7 192l22.6 22.6 160 160L224 397.3z"/>
+            d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/>
         ) : (
           <path
             d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
