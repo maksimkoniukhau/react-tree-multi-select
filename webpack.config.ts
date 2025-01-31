ffcc1fda2f90e75f8d3c87c0df68a47cd0a1d382
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2025-01-31 15:39:11 +0100

    Add only specific types to tsconfig in order for dts-bundle-generator to work

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,48 +3,48 @@ import {resolve} from 'path';
 import {Configuration} from 'webpack';
 
 const config: Configuration = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true,
-                        happyPackMode: true,
-                    },
-                },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {implementation: sass},
-                    },
-                ],
-            }
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            happyPackMode: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {implementation: sass},
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'index.umd.js',
-        path: resolve(__dirname, 'dist'),
-        libraryTarget: "umd",
-        library: 'rts',
-        umdNamedDefine: true,
-        clean: true,
-    },
-    externals: {
-        react: 'react',
-    },
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.umd.js',
+    path: resolve(__dirname, 'dist'),
+    libraryTarget: "umd",
+    library: 'rts',
+    umdNamedDefine: true,
+    clean: true,
+  },
+  externals: {
+    react: 'react',
+  },
 };
 
 export default config;
