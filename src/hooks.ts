269e31add539f63b7981a0c7d07eb42d191a1420
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2025-05-08 17:08:27 +0200

    Remove unneeded hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,45 +1,4 @@
-import {RefObject, useEffect, useLayoutEffect, useState} from 'react';
-
-export const useResizeObserver = <T extends Element>(
-  target: RefObject<T | null> | T | null,
-  callback: (entries: ResizeObserverEntry[], observer: ResizeObserver) => void
-): void => {
-
-  useLayoutEffect(() => {
-    const targetElement = target && 'current' in target ? target.current : target as Element;
-    if (!targetElement) return;
-
-    const resizeObserver = new ResizeObserver(callback);
-
-    resizeObserver.observe(targetElement);
-
-    return () => resizeObserver.unobserve(targetElement);
-  }, [target]);
-};
-
-export const useElementSize = <T extends HTMLElement>(target: RefObject<T | null> | T | null): [number, number] => {
-
-  const [size, setSize] = useState<[number, number]>(() => {
-    const targetEl = target && 'current' in target ? target.current : target as HTMLElement;
-    return targetEl
-      ? [targetEl.offsetWidth, targetEl.offsetHeight]
-      : [0, 0];
-  });
-
-  useLayoutEffect(() => {
-    const targetElement = target && 'current' in target ? target.current : target as HTMLElement;
-    if (!targetElement) return;
-
-    setSize([targetElement.offsetWidth, targetElement.offsetHeight]);
-  }, [target]);
-
-  useResizeObserver(target, (entries: ResizeObserverEntry[]) => {
-    const targetElement = entries[0].target as HTMLElement;
-    setSize([targetElement.offsetWidth, targetElement.offsetHeight]);
-  });
-
-  return size;
-};
+import {RefObject, useEffect} from 'react';
 
 export const useOnClickOutside = <T extends HTMLElement>(
   target: RefObject<T | null> | T | null,
