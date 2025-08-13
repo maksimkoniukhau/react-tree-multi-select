96cbcff8a8480c322159f1ffe0fb051e316e18ea
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2025-08-13 20:46:33 +0200

    Remove create-react-app website

--- apps/website/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App', () => {
-  render(<App />);
-  const element = screen.getAllByText(/REACT TREE MULTI SELECT/i)[0];
-  expect(element).toBeInTheDocument();
-});
