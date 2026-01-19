07bcc973b52d947b71dbebf0d82e153e2a0d16b6
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2026-01-19 21:16:39 +0100

    Fix useItemsResizeObserver to work in nextjs app on load page when dropdown is open (create ResizeObserver in client code, but not on SSR)

--- packages/react-tree-multi-select/src/hooks/useItemsResizeObserver.ts
+++ packages/react-tree-multi-select/src/hooks/useItemsResizeObserver.ts
@@ -4,44 +4,48 @@ export const useItemsResizeObserver = (
   callback: (index: number, size: { width: number; height: number }) => void
 ) => {
   const observerRef = useRef<ResizeObserver>(null);
-  if (!observerRef.current) {
-    observerRef.current = new ResizeObserver(entries => {
-      for (const entry of entries) {
-        const index = elementMap.current.get(entry.target);
-        if (index !== undefined) {
-          const width = entry.borderBoxSize[0].inlineSize;
-          const height = entry.borderBoxSize[0].blockSize;
-          if (width !== 0 || height !== 0) {
-            callback(index, {width, height});
-          }
-        }
-      }
-    });
-  }
-
   const elementMap = useRef(new Map<Element, number>());
 
-  const callbackRef = useRef(callback);
-  callbackRef.current = callback;
+  const getObserver = (): ResizeObserver => {
+    if (!observerRef.current) {
+      observerRef.current = new ResizeObserver(entries => {
+        for (const entry of entries) {
+          const index = elementMap.current.get(entry.target);
+          if (index !== undefined) {
+            const width = entry.borderBoxSize[0].inlineSize;
+            const height = entry.borderBoxSize[0].blockSize;
+            if (width !== 0 || height !== 0) {
+              callback(index, {width, height});
+            }
+          }
+        }
+      });
+    }
+
+    return observerRef.current;
+  };
 
   useEffect(() => {
-    return () => observerRef.current?.disconnect();
+    return () => {
+      observerRef.current?.disconnect();
+      observerRef.current = null;
+    };
   }, []);
 
   const observeItem = (element: HTMLElement | null, index: number): void => {
-    if (!element || !observerRef.current) {
+    if (!element) {
       return;
     }
     elementMap.current.set(element, index);
-    observerRef.current.observe(element);
+    getObserver().observe(element);
   };
 
   const unobserveItem = (element: HTMLElement | null): void => {
-    if (!element || !observerRef.current) {
+    if (!element) {
       return;
     }
     elementMap.current.delete(element);
-    observerRef.current.unobserve(element);
+    observerRef.current?.unobserve(element);
   };
 
   return {observeItem, unobserveItem};
