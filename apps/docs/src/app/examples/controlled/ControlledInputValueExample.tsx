de3b4bb31a458c5282936664aaaef7c23329d1eb
Maksim Koniukhau <koniukhau.maksim@gmail.com>
2026-02-08 17:43:43 +0100

    Add controlled/uncontrolled inputValue examples

---
+++ apps/docs/src/app/examples/controlled/ControlledInputValueExample.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React, {FC, memo, useState} from 'react';
+import {TreeMultiSelect, TreeNode} from 'react-tree-multi-select';
+import {getTreeNodeData} from '@/utils/utils';
+import {Checkbox} from '@/shared-components/Checkbox';
+
+export const ControlledInputValueExample: FC = memo(() => {
+
+  const [data] = useState<TreeNode[]>(getTreeNodeData());
+  const [value, setValue] = useState<string>('spring');
+  const [open, setOpen] = useState<boolean>(false);
+  const [keepOpen, setKeepOpen] = useState<boolean>(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setValue(event.currentTarget.value);
+  };
+
+  const handleDropdownToggle = (isOpen: boolean): void => {
+    setOpen(isOpen || keepOpen);
+  };
+
+  return (
+    <div className="controlled-example">
+      <div style={{display: 'flex'}}>
+        <input value={value} onChange={handleInputChange}/>
+        <Checkbox label="Keep dropdown open" checked={keepOpen} onChange={(v) => setKeepOpen(v)}/>
+      </div>
+      <TreeMultiSelect
+        data={data}
+        inputValue={value}
+        isDropdownOpen={open}
+        onInputChange={handleInputChange}
+        onDropdownToggle={handleDropdownToggle}
+      />
+    </div>
+  );
+});
